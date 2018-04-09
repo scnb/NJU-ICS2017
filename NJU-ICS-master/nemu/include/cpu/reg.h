00ac8d7be037125c6785ae709dbe67dc592b2b53
sc <18633910528@163.com>
2018-04-09 23:39:02 +0800

    undo rtlreg_t

--- NJU-ICS-master/nemu/include/cpu/reg.h
+++ NJU-ICS-master/nemu/include/cpu/reg.h
@@ -28,7 +28,7 @@ typedef struct {
    * in PA2 able to directly access these registers.
    */
  // rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;   
-  rtlreg_t eax, edx, ecx, ebx, ebp, esi, edi, esp;
+  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 
   vaddr_t eip;
 
