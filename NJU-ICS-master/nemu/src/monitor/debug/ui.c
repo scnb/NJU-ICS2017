74d2e6b33da56a41820cdb179c6183725799f000
tracer-ics2017 <tracer@njuics.org>
2018-04-14 00:16:27 +0800

    >  compile 161220000 neuqsc Linux neuqsc 4.4.0-119-generic #143-Ubuntu SMP Mon Apr 2 16:08:24 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  00:16:27 up 14:56,  1 user,  load average: 0.51, 0.16, 0.08 5867b531723fa0db247f5ebb2b944f558ec841ea

--- NJU-ICS-master/nemu/src/monitor/debug/ui.c
+++ NJU-ICS-master/nemu/src/monitor/debug/ui.c
@@ -141,7 +141,7 @@ static int cmd_x(char *args)
 	{
 	   /* read 4 bytes once */
 	   /* Note:the address must be in dec format, even though you entered in hex format */
-	   	printf("0x%d:		%8X		%d\n", address_hex,vaddr_read(address_dec, 4), vaddr_read(address_dec, 4));
+	   	printf("0x%d:		%#x		%d\n", address_hex,vaddr_read(address_dec, 4), vaddr_read(address_dec, 4));
 		/* add 4 bytes to the address */
 		address_hex += 4;
 		address_dec += 4;
